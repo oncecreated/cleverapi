f2f2008c721c40843dc6b518f14395fb99d304e5
oncecreated <28735725+oncecreated@users.noreply.github.com>
2018-06-30 11:51:05 +0300

    set encoding for readme file in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
