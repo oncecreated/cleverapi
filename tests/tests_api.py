db129c2a0eea8a2414176f93e9eb0033d8718d47
oncecreated <28735725+oncecreated@users.noreply.github.com>
2018-06-30 11:45:30 +0300

    harcode hashes testdata in test

--- tests/tests_api.py
+++ tests/tests_api.py
@@ -5,25 +5,32 @@ sys.path.append('../')
 
 import pytest
 from cleverapi import CleverApi
-from testdata import hashes
+
 
 @pytest.fixture
 def api():
     return CleverApi(None)
+
 
 def test_hash_action(api):
     api.user_id = "91670994"
     api.device_id = "77a3af1dbf002b1b"
     hash = api.get_hash(["6"])
 
-    assert hash == hashes.action_test_hash
+    assert hash == "8cc151519e41f560bf93b02278413875#"
+    "5c75528a585768893d29c137b342057f#"
+    "34f1d74e8991b6687a296f6a8c8c92a7"
+
 
 def test_hash_answer(api):
     api.user_id = "91670994"
     api.device_id = "77a3af1dbf002b1b"
     hash = api.get_hash(["215", "18"])
 
-    assert hash == hashes.answer_test_hash
+    assert hash == "f892054ed677ea5a654aa25606ea045d#"
+    "5c75528a585768893d29c137b342057f#"
+    "34f1d74e8991b6687a296f6a8c8c92a7"
+
 
 def test_send_answer_with_zero_game_id(api):
     with pytest.raises(Exception) as exception:
