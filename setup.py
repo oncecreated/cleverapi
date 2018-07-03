37614ac0a509e1a0b07e0198234d7e517ed5795f
oncecreated <28735725+oncecreated@users.noreply.github.com>
2018-07-04 00:04:53 +0300

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="cleverapi",
-    version="0.2",
+    version="0.2.1",
     author="Oncecreated",
     description="Python Clever Api",
     long_description=long_description,
