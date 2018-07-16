5244d8a53edfe3cd015ff2f2d0eb04a02f898a2a
Oncecreated <28735725+oncecreated@users.noreply.github.com>
2018-07-16 23:37:06 +0300

    Refactoring CleverApi, now it child of BaseCleverApi, more testing, more flexible

--- cleverapi/clever_api.py
+++ cleverapi/clever_api.py
@@ -6,43 +6,31 @@ import requests
 
 
 class BaseCleverApi():
-    def __init__(self, access_token, verison="5.73"):
+    def __init__(self, access_token, version="5.73"):
         self.access_token = access_token
-        self.api_verison = verison
+        self.api_version = version
 
         self.device_id = uuid.uuid4().hex[:16]
         self.user_id = None
 
-        self.session = requests.Session()
-        self.session.headers.update({
-            "User-Agent": "Клевер/2.1.1 (Redmi Note 3; "
-            "Android 23; Scale/3.00; VK SDK 1.6.8; com.vk.quiz)".encode(
-                "utf-8")
-        })
-
-    def request(self, method, payload: dict):
-        raise NotImplementedError()
-
-    def get_long_poll(self, owner_id, video_id):
-        payload = {"owner_id": owner_id, "video_id": video_id}
-        return self.request("video.getLongPollServer", payload)
+    def get_longpoll(self, owner_id, video_id):
+        data = {"owner_id": owner_id, "video_id": video_id}
+        return "video.getLongPollServer", data
 
     def get_start_data(self):
-        payload = {
+
+        data = {
             "build_ver": "40031",
             "need_leaderboard": "0",
             "func_v": "4",
             "lang": "ru",
             "https": "1"
         }
-        return self.request("execute.getStartData", payload)
+
+        return "execute.getStartData", data
 
     def get_user(self):
-        return self.request("users.get", dict())
-
-    def __resolve_user_id(self):
-        user = self.get_user()
-        self.user_id = user[0]["id"]
+        return "users.get"
 
     def get_hash(self, params: list):
         ids = ("".join(map(str, params)) + "3aUFMZGRCJ").encode("utf-8")
@@ -56,54 +44,16 @@ class BaseCleverApi():
 
         return "{}#{}#{}".format(ids_hash, user_hash, device_hash)
 
-    def use_proxy(self, address):
-        self.session.proxies = {
-            "http": address,
-            "https": address
-        }
-
-    def remove_proxy(self):
-        self.session.proxies = dict()
-
     def send_action(self, action_id):
-        if not self.user_id:
-            self.__resolve_user_id()
-
         hash = self.get_hash([action_id])
-        payload = {"action_id": action_id, "hash": hash}
+        data = {"action_id": action_id, "hash": hash}
 
-        return self.request("streamQuiz.trackAction", payload)
-
-    def send_action_watched_game(self):
-        return self.send_action(1)
-
-    def send_action_join_game(self):
-        return self.send_action(2)
-
-    def send_action_answer_correct(self):
-        return self.send_action(3)
-
-    def send_action_win_game(self):
-        return self.send_action(4)
-
-    def send_action_invite_friend(self):
-        return self.send_action(5)
-
-    def send_action_community_notify(self):
-        return self.send_action(6)
-
-    def send_action_aliexpress_auth(self):
-        return self.send_action(8)
+        return "streamQuiz.trackAction", data
 
     def send_answer(self, coins_answer, game_id, answer_id, question_id):
-        if game_id == 0:
-            raise Exception("game_id must be non-zero")
-
-        if not self.user_id:
-            self.__resolve_user_id()
-
         hash = self.get_hash([game_id, question_id])
-        payload = {
+
+        data = {
             "answer_id": answer_id,
             "question_id": question_id,
             "device_id": self.device_id,
@@ -111,32 +61,75 @@ class BaseCleverApi():
         }
 
         if coins_answer is True:
-            payload["coins_answer"] = True
+            data["coins_answer"] = True
 
-        return self.request("streamQuiz.sendAnswer", payload)
+        return "streamQuiz.sendAnswer", data
 
     def get_gifts(self):
-        return self.request("execute.getGifts", dict())
+        return "execute.getGifts"
 
-    def purchase_gitf(self, gift_id):
-        payload = {"gift_id": gift_id}
-        return self.request("streamQuiz.purchaseGift", payload)
+    def purchase_gift(self, gift_id):
+        data = {"gift_id": gift_id}
+        return "streamQuiz.purchaseGift", data
 
     def use_extra_life(self):
-        return self.request("streamQuiz.useExtraLife", dict())
+        return "streamQuiz.useExtraLife"
 
 
 class CleverApi(BaseCleverApi):
+    def __init__(self, access_token, version="5.73"):
+        super().__init__(access_token, version=version)
 
-    def request(self, method, payload: dict):
-        payload["access_token"] = self.access_token
-        payload["v"] = self.api_verison
-        payload["lang"] = "ru"
-        payload["https"] = 1
+        self.session = requests.Session()
+        self.session.headers.update({
+            "User-Agent": "Клевер/2.1.1 (Redmi Note 3; "
+            "Android 23; Scale/3.00; VK SDK 1.6.8; com.vk.quiz)".encode("utf-8")
+        })
+
+    def fetch(self, method, data=dict()):
+        data.update({
+            "access_token": self.access_token,
+            "v": self.api_version, 
+            "lang": "ru",
+            "https": 1
+        })
+
         content = self.session.post("https://api.vk.com/method/{}"
-                                    .format(method), data=payload).json()
+                                    .format(method), data=data).json()
 
         if "error" in content:
             raise Exception("api response has error: " + json.dumps(content))
 
         return content["response"]
+
+    def get_longpoll(self, owner_id, video_id):
+        method, data = super().get_longpoll(owner_id, video_id)
+        return self.fetch(method, data)
+
+    def get_start_data(self):
+        method, data = super().get_start_data()
+        return self.fetch(method, data)
+
+    def get_user(self):
+        method = super().get_user()
+        return self.fetch(method)
+
+    def send_action(self, action_id):
+        method, data = super().send_action(action_id)
+        return self.fetch(method, data)
+
+    def send_answer(self, coins_answer, game_id, answer_id, question_id):
+        method, data = super().send_answer(coins_answer, game_id, answer_id, question_id)
+        return self.fetch(method, data)
+
+    def get_gifts(self):
+        method = super().get_gifts()
+        return self.fetch(method)
+
+    def purchase_gitf(self, gift_id):
+        method, data = super().purchase_gift(gift_id)
+        return self.fetch(method, data)
+
+    def use_extra_life(self):
+        method = super().use_extra_life()
+        return self.fetch(method)
