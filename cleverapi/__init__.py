b29b5c6b5c6d982546289c2025f2b8857e01b926
Oncecreated <28735725+oncecreated@users.noreply.github.com>
2018-07-20 21:36:18 +0300

    Add enum Action with send_action options

--- cleverapi/__init__.py
+++ cleverapi/__init__.py
@@ -1,3 +1,4 @@
 from .clever_api import BaseCleverApi, CleverApi, AsyncCleverApi
 from .clever_longpoll import CleverLongPoll
 from .connector import Connector
+from .action import Action
---
+++ cleverapi/action.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Action(Enum):
+    WATCHED_GAME = 1,
+    JOIN_GAME = 2,
+    ANSWER_CORRECT = 3,
+    WIN_GAME = 4,
+    INVITE_FRIEND = 5,
+    COMMUNITY_NOTIFY = 6,
