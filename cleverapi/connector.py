827fbcd580e4ca1cf2474451c95986d6f41bd594
Oncecreated <28735725+oncecreated@users.noreply.github.com>
2018-08-23 04:01:17 +0300

    Update user-agent in async connector

--- cleverapi/connector.py
+++ cleverapi/connector.py
@@ -6,8 +6,8 @@ class Connector():
 
     async def __aenter__(self):
         headers = {
-            "User-Agent": str("Клевер/2.1.1 (Redmi Note 3; "
-            "Android 23; Scale/3.00; VK SDK 1.6.8; com.vk.quiz)")
+            "User-Agent": str("Клевер/2.3.3 (Redmi Note 5; "
+            "Android 28; VK SDK 1.6.8; com.vk.quiz)")
         }
         self.session = aiohttp.ClientSession(headers=headers)
 
